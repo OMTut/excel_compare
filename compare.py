47dae7154a8ce0bd362d1def9a0c0078a9e661ac
OMTut <j_tuttle04@hotmail.com>
2023-05-17 12:30:57 -0500

    crude comparison of two files

---
+++ compare.py
@@ -0,0 +1,26 @@
+import pandas as pd
+import openpyxl
+
+try:
+    import numpy as np
+except ImportError:
+    print('Numpy is not installed')
+
+# Read the excel file
+workbook1 = openpyxl.load_workbook(filename = 'data/supermarkt_sales_1.xlsx')
+workbook2 = openpyxl.load_workbook(filename = 'data/supermarkt_sales_2.xlsx')
+
+# Get the data from both files using pandas
+sheet1 = workbook1.active
+data1 = pd.DataFrame(sheet1.values)
+
+sheet2 = workbook2.active
+data2 = pd.DataFrame(sheet2.values)
+
+# Compare the data
+comparison_values = data1.values == data2.values
+rows, cols = np.where(comparison_values == False)
+
+#Generate report showing the cells that differ
+for row, col in zip(rows, cols):
+    print(f'Cells ({row + 1}, {col + 1}) are different')
